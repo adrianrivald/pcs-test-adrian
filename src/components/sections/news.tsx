815ca5b453b366485ed428c05d09754aad40282a
adrianrivald <adrianrivaldy@gmail.com>
2025-07-25 13:23:39 +0700

    move testimonials to constants

--- src/components/sections/news.tsx
+++ src/components/sections/news.tsx
@@ -1,46 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const testimonials = [
-  {
-    name: "Ana Riswati",
-    day: "Senin",
-    date: "30 Mei 2022",
-    image: "https://api.dicebear.com/7.x/pixel-art/svg?seed=avatar2", // Replace with actual avatar
-    content: [
-      "Kalimat 1 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 2 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 3 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 4 - Lorem ipsum dolor sit amet consec",
-    ],
-  },
-  {
-    name: "Adrian R.",
-
-    day: "Senin",
-    date: "30 Mei 2022",
-    image: "https://api.dicebear.com/7.x/pixel-art/svg?seed=avatar2", // Replace with actual avatar
-    content: [
-      "Kalimat 1 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 2 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 3 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 4 - Lorem ipsum dolor sit amet consec",
-    ],
-  },
-  {
-    name: "Rivaldy A.",
-    day: "Senin",
-    date: "30 Mei 2022",
-    image: "https://api.dicebear.com/7.x/pixel-art/svg?seed=avatar2", // Replace with actual avatar
-    content: [
-      "Kalimat 1 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 2 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 3 - Lorem ipsum dolor sit amet consec",
-      "Kalimat 4 - Lorem ipsum dolor sit amet consec",
-    ],
-  },
-];
+import { testimonials } from "../../constants/testimonials";
 
 const News = () => {
   const settings = {
---
+++ src/constants/testimonials.ts
@@ -0,0 +1,40 @@
+
+export const testimonials = [
+  {
+    name: "Ana Riswati",
+    day: "Senin",
+    date: "30 Mei 2022",
+    image: "https://api.dicebear.com/7.x/pixel-art/svg?seed=avatar2", // Replace with actual avatar
+    content: [
+      "Kalimat 1 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 2 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 3 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 4 - Lorem ipsum dolor sit amet consec",
+    ],
+  },
+  {
+    name: "Adrian R.",
+
+    day: "Senin",
+    date: "30 Mei 2022",
+    image: "https://api.dicebear.com/7.x/pixel-art/svg?seed=avatar2", // Replace with actual avatar
+    content: [
+      "Kalimat 1 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 2 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 3 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 4 - Lorem ipsum dolor sit amet consec",
+    ],
+  },
+  {
+    name: "Rivaldy A.",
+    day: "Senin",
+    date: "30 Mei 2022",
+    image: "https://api.dicebear.com/7.x/pixel-art/svg?seed=avatar2", // Replace with actual avatar
+    content: [
+      "Kalimat 1 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 2 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 3 - Lorem ipsum dolor sit amet consec",
+      "Kalimat 4 - Lorem ipsum dolor sit amet consec",
+    ],
+  },
+];
